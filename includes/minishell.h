7220a9a6f54cd667ffb44187534ca0b833d5349d
cerdelen <cerdelen@cerdelens-Air.fritz.box>
2022-04-08 16:47:59 +0200

    added beginning of struct

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,7 +3,14 @@
 # define MINISHELL_H
 
 # include <stdlib.h>
-
+# include "libft.h"
 # include <unistd.h>
+# include <errno.h>
+
+typedef struct s_ms_data
+{
+	char	*current_path;
+	
+}	t_ms_data;
 
 #endif
