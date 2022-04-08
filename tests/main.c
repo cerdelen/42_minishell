01caa19b43bea03ebe1b81b986c0c8d3b0883bfe
kis619 <kis619@abv.bg>
2022-04-08 19:04:48 +0200

    modified ft_split to take into cosideration quotation marks

--- tests/main.c
+++ tests/main.c
@@ -6,7 +6,7 @@
 /*   By: kmilchev <kmilchev@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 15:46:44 by kmilchev          #+#    #+#             */
-/*   Updated: 2022/04/08 15:53:34 by kmilchev         ###   ########.fr       */
+/*   Updated: 2022/04/08 16:50:08 by kmilchev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,17 @@
 
 int main()
 {
-	char *string;
-	string = "whatev||'|'||er";
-	printf("Quotes are closed: %d\n", quotes_are_closed(string));
-	printf("Number of pipes: %d\n", count_chars(string, '|'));
+	// char *string;
+	// string = "whatev||'|'||er";
+	// printf("Quotes are closed: %d\n", quotes_are_closed(string));
+	// printf("Number of pipes: %d\n", count_chars(string, '|'));
+	int i = 1;
+	while(i < 10)
+	{
+		if (1 == 1)
+		{
+			i++;
+			continue ;
+		}
+	}
 }
--- tests/tests_for_quotes_are_closed.c
+++ tests/tests_for_quotes_are_closed.c
@@ -6,11 +6,36 @@
 /*   By: kmilchev <kmilchev@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 15:44:55 by kmilchev          #+#    #+#             */
-/*   Updated: 2022/04/08 15:45:03 by kmilchev         ###   ########.fr       */
+/*   Updated: 2022/04/08 18:44:52 by kmilchev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdbool.h>
+
+bool quotes_are_closed(char c)
+{
+	static bool d_quotes_open = false;
+	static bool s_quotes_open = false;
+
+	if (c == '\'' && !d_quotes_open)
+	{
+		if (s_quotes_open)
+			s_quotes_open = false;
+		else
+			s_quotes_open = true;
+	}
+	else if (c == '\"' && !s_quotes_open)
+	{
+		if (d_quotes_open)
+			d_quotes_open = false;
+		else
+			d_quotes_open = true;
+	}
+	if (!d_quotes_open && !s_quotes_open)
+		return (true);
+	return (false);
+}
 
 int main ()
 {
