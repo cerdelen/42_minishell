38305c74e8f6c8752d5810754fbca8ce2ea23099
cerdelen <cerdelen@cerdelens-Air.fritz.box>
2022-04-09 13:37:42 +0200

    finishing cd ... still has testing main

--- src/cer/minishell.h
+++ src/cer/minishell.h
@@ -1,1 +1,14 @@
-#include ""
+#ifndef Minishell_h
+
+#define Minishell_h
+
+
+#include <errno.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <limits.h>
+#include <string.h>
+#include <stdlib.h>
+#include "../../includes/libft.h"
+
+#endif
