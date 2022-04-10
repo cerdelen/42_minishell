51de20dd94bb76ac968af1b1b595b446d10dbbae
cerdelen <cerdelen@cerdelens-Air.fritz.box>
2022-04-10 11:34:40 +0200

    update header

--- includes/minishell.h
+++ includes/minishell.h
@@ -2,15 +2,28 @@
 
 # define MINISHELL_H
 
-# include <stdlib.h>
 # include "libft.h"
+# include <stdlib.h>
 # include <unistd.h>
 # include <errno.h>
+# include <stdio.h>
+# include <limits.h>
+# include <string.h>
+# include <fcntl.h>
 
 typedef struct s_ms_data
 {
-	char	*current_path;
-	
+	char			**env;
+	(your struct)	*commands;
+	int				command_amt;
+	char			*in_f;		      //Null if no < input
+	char			*out_f;
+	int				output_type;    //0 to std  1 >  2 >>
+	bool			here_doc;
+	char			*limiter;
 }	t_ms_data;
+
+char	*ft_tripple_strjoin(char *s1, char *s2, char *s3);
+
 
 #endif
