e5d30ffd5f7f34f4ec78ab209bc06e9f82af40fa
kis619 <kis619@abv.bg>
2022-04-12 13:12:16 +0200

    moved error hanfling in a new file

---
+++ parsing/error_management.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error_management.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmilchev <kmilchev@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/11 21:16:43 by kmilchev          #+#    #+#             */
+/*   Updated: 2022/04/11 22:09:21 by kmilchev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/parsing.h"
+
+int errors(char *string)
+{
+	if (!quotes_are_closed(string))
+	{
+		// printf("quotes_error: Quotes are not closed\n");
+		return (printf("quotes_error: Quotes are not closed\n"), 1);
+	}
+	if (double_pipe(string))
+	{
+		printf("double_pipe_error: Only single pipes allowed\n");
+		return (2);
+	}
+	if (multiple_redirection(string, '<') || multiple_redirection(string, '>'))
+	{
+		printf("multiple angular brace: only >, <, >>, and << allowed\n");
+		return (3);
+	}
+	return (0);
+}
