441e869afefbdbd8dfee4aca6bbf6895fcb8b990
kmilchev <kmilchev@student.42wolfsburg.de>
2022-04-18 13:53:08 +0200

    norminetted it, removed an obsolete function

--- parsing/print_utils.c
+++ parsing/print_utils.c
@@ -6,55 +6,43 @@
 /*   By: kmilchev <kmilchev@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 10:03:38 by kmilchev          #+#    #+#             */
-/*   Updated: 2022/04/16 21:54:47 by kmilchev         ###   ########.fr       */
+/*   Updated: 2022/04/18 13:49:54 by kmilchev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/parsing.h"
 
-void print_cmd_struct(t_full_pipe cmd)
+void	print_cmd_struct(t_full_pipe cmd)
 {
-	// int i = 0;
-	
 	printf("-----------\nCMD_FLAGS:\n");
 	print_2d_array(cmd.cmd_flags);
 	printf("-----------\nHERE_DOC:\n");
 	print_2d_array(cmd.here_doc);
 	printf("-----------\nRED_IN:\n");
-	print_2d_array(cmd.reddir_in);
+	print_2d_array(cmd.input);
 	printf("-----------\nRED_OUT:\n");
-	print_2d_array(cmd.reddir_out);
+	print_2d_array(cmd.output);
 	printf("-----------\nRED_OUT_APP:\n");
-	print_2d_array(cmd.reddir_out_app);
-	// while(cmd.cmd_flags[i])
-	// 	printf("CMD_FLAG: %s\n", cmd.cmd_flags[i++]);
-	// i = 0;	
-	// while(cmd.here_doc[i])
-	// 	printf("HERE_DOC: %s\n", cmd.here_doc[i++]);
-	// i = 0;	
-	// while(cmd.reddir_in[i])
-	// 	printf("REDIRECT_INPUT: %s\n", cmd.reddir_in[i++]);
-	// i = 0;	
-	// while(cmd.reddir_out[i])
-	// 	printf("REDIRECT_OUTPUT: %s\n", cmd.reddir_out[i++]);
-	// i = 0;	
-	// while(cmd.reddir_out_app[i])
-	// 	printf("REDIRECT_OUT_APP: %s\n", cmd.reddir_out_app[i++]);
+	print_2d_array(cmd.out_append);
 }
 
-void print_cmd_struct_arr(t_full_pipe *arr, int command_amt)
+void	print_cmd_struct_arr(t_full_pipe *arr, int command_amt)
 {
-	int i = 0;
-	while(i < command_amt)
+	int	i;
+
+	i = 0;
+	while (i < command_amt)
 	{
 		print_cmd_struct(arr[i]);
 		printf("**********************\n");
 		i++;
 	}
 }
-void print_env_struct(t_env *envv, int i)
+
+void	print_env_struct(t_env *envv, int i)
 {
-	int j;
+	int	j;
+
 	j = 0;
 	while (j < i)
 	{
@@ -64,49 +52,14 @@ void print_env_struct(t_env *envv, int i)
 	}
 }
 
-void print_2d_array(char **arr)
+void	print_2d_array(char **arr)
 {
-	int i = 0;
+	int	i;
+
+	i = 0;
 	while (arr[i])
 	{
 		printf("%sX\n", arr[i]);
-		i++;
-	}
-}
-
-void print_struct_array(t_full_pipe *arr, int n_elements)
-{
-	int i;
-
-	i = 0;
-	while (i < n_elements)
-	{
-		printf("\nELEMENT %d ---------------------\n", i);
-		if (arr[i].reddir_in)
-		{
-			printf("Redirection in:\n");
-			print_2d_array(arr[i].reddir_in);
-		}
-		if (arr[i].reddir_out)
-		{
-			printf("Redirection out:\n");
-			print_2d_array(arr[i].reddir_out);
-		}
-		if (arr[i].reddir_out_app)
-		{
-			printf("Double Redirection out:\n");
-			print_2d_array(arr[i].reddir_out_app);
-		}
-		if (arr[i].here_doc)
-		{
-			printf("Here_doc Redirection :\n");
-			print_2d_array(arr[i].here_doc);
-		}
-		if (arr[i].cmd_flags)
-		{
-			printf("COMMAND and FLAGS: \n");
-			print_2d_array(arr[i].cmd_flags);
-		}
 		i++;
 	}
 }
