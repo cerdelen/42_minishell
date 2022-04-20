929893ba6827772555d751ce92bca6ae8752fa06
kmilchev <kmilchev@student.42wolfsburg.de>
2022-04-20 19:16:30 +0200

    norm

--- parsing/error_management.c
+++ parsing/error_management.c
@@ -6,7 +6,7 @@
 /*   By: kmilchev <kmilchev@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 21:16:43 by kmilchev          #+#    #+#             */
-/*   Updated: 2022/04/20 19:12:14 by kmilchev         ###   ########.fr       */
+/*   Updated: 2022/04/20 19:16:14 by kmilchev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,15 @@
 //Returns 0 if there are no errors
 int	errors(char *string)
 {
-	if (string[0] == '|' || string[ft_strlen(string) - 1] == '|' || ft_strnstr(string, "| |",  ft_strlen(string)))
+	if (string[0] == '|' || string[ft_strlen(string) - 1] == '|'
+		|| ft_strnstr(string, "| |", ft_strlen(string)))
 		return (printf("syntax_error: near unexpected token `|'\n"), 1);
 	if (!all_quotes_are_closed(string))
 		return (printf("quotes_error: Quotes are not closed\n"), 2);
 	if (double_pipe(string))
 		return (printf("double_pipe_error: Only single pipes allowed\n"), 3);
 	if (multiple_redirection(string, '<') || multiple_redirection(string, '>'))
-		return (printf("multiple_angular_brace: only >, <, >>, and << allowed\n"), 4);
+		return (printf("multiple_<>_error: only >, <, >>, and << allowed\n"), 4);
 	if (wrong_angular_braces(string))
 		return (printf("<_>error: inappropriate use of '>' or '<'\n"), 5);
 	return (0);
