e0087a04123359e07286f215cea5a127a6cd9b50
kmilchev <kmilchev@student.42wolfsburg.de>
2022-04-24 11:17:53 +0200

    included parsing.h in the minishell header

--- includes/minishell.h
+++ includes/minishell.h
@@ -4,6 +4,7 @@
 
 # include <stdbool.h>
 # include "libft.h"
+# include "parsing.h"
 # include <stdlib.h>
 # include <unistd.h>
 # include <errno.h>
@@ -14,6 +15,7 @@
 # include <stdbool.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+
 
 typedef struct s_ms_data
 {
