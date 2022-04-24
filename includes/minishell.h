62fbec147ec8884c40455e4044353d51636a97a7
kmilchev <kmilchev@student.42wolfsburg.de>
2022-04-24 11:11:24 +0200

    updated header

--- includes/minishell.h
+++ includes/minishell.h
@@ -2,15 +2,36 @@
 
 # define MINISHELL_H
 
-# include <stdlib.h>
+# include <stdbool.h>
 # include "libft.h"
+# include "parsing.h"
+# include <readline/readline.h>
+# include <readline/history.h>
+// # include "get_next_line.h"
+// # include "commands_and_exec.h"
+# include <stdlib.h>
 # include <unistd.h>
 # include <errno.h>
+# include <stdio.h>
+# include <limits.h>
+# include <string.h>
+# include <fcntl.h>
+# include <stdbool.h>
+
+// typedef struct s_complex_command
+// {
+// 	char	**input;
+// 	char	**output;
+// 	char	**cmd_flags;
+// }	t_cmd;
 
 typedef struct s_ms_data
 {
-	char	*current_path;
-	
+	char				**env;
+	t_cmd				*command;
+	int					command_amt;
+	pid_t				p_id;
+	int					*exit_codes;
 }	t_ms_data;
 
 #endif
