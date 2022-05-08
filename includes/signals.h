45be98c2997826361ede4fa8d8c294b7c75f3e3e
kmilchev <kmilchev@student.42wolfsburg.de>
2022-05-08 16:17:59 +0200

    remode unnecessary includes

--- includes/signals.h
+++ includes/signals.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signals.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cerdelen <cerdelen@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kmilchev <kmilchev@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/24 13:25:44 by kmilchev          #+#    #+#             */
-/*   Updated: 2022/05/08 15:13:09 by cerdelen         ###   ########.fr       */
+/*   Updated: 2022/05/08 15:49:00 by kmilchev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 # define SIGNALS_H
 
 # include <signal.h>
-# include <stdlib.h>
-# include "minishell.h"
 
 void			handle_sigs_interactive(void);
 void			handle_child_signals(void);
