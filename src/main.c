5ff894c641ad46361099490b6393d8c4a3a26072
Cedric Erdelen <cerdelen@c2s15d88.42wolfsburg.de>
2022-05-08 12:15:49 +0200

    getting rid of test printfs

--- src/main.c
+++ src/main.c
@@ -3,15 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kmilchev <kmilchev@student.42wolfsburg.de> +#+  +:+       +#+        */
+/*   By: cerdelen <cerdelen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/24 09:46:07 by kmilchev          #+#    #+#             */
-/*   Updated: 2022/05/07 21:09:46 by kmilchev         ###   ########.fr       */
+/*   Updated: 2022/05/08 12:14:23 by cerdelen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
-
 
 static void	sigint_handler(int signal)
 {
@@ -50,23 +49,22 @@ int	main(int argc, char *argv[], char *env[])
 {
 	t_ms_data			data;
 	char				*line;
-	
+
 	data.exit_codes = 0;
 	data.env = env_copy(env);
-
 	handle_sigs_interactive();
 	if (argc != 1 && argv)
 		return (printf("KISCER_SHELL does not take arguments\n"), 0);
-	while(1)
+	while (1)
 	{
 		line = readline(WHATEVS);
 		if (!line)
 		{
-			printf("exit\n");
+			write(2, "exit\n", 5);
 			break ;
 		}
 		if (line[0] == 0)
-			continue;
+			continue ;
 		if (ft_strlen(line) > 0)
 			add_history(line);
 		if (parse(&line, &data))
